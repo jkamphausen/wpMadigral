dd4cd9f0f38941cfcdce327508be3cf7b00f3bbc
KamphausenConsulting <juliankamphausen@icloud.com>
2018-03-08 16:35:41 +0100

    static index.php
    added static index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="e.joesting">
+
+        <title>Rheydter Madrigalchor - Seit 1966</title>
+        <link href="<?php echo get_bloginfo( 'template_directory' );?>/css/design.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <?php wp_head();?>
+    </head>
+    <body >
+        <div id="rahmen">
+            <div id="kopf">
+                <div id="kopfinnen">  
+                </div> 
+			</div>
+            <div id="menuleiste">
+                <div id="menuinnen"> 
+                    <ul>
+                        <li class="aktiv"><a href="index.htm">Home</a></li>
+                        <li><a href="kontakt.htm">Kontakt</a></li>
+                        <li><a href="impressum.htm">Impressum</a></li>
+                    </ul>
+    		      </div>
+            </div>
+            <div id="linkespalte">
+                <div id="linksinnen">
+                    <ul>
+                        <li><a href="chor.htm">Über uns</a></li>
+                        <li><a href="repertoire.htm">Repertoire</a></li>
+                        <li><a href="termine.htm">Aktuell</a></li>
+                        <li><a href="hoerproben.htm">Hörproben</a></li>
+                        <li><a href="chorfahrten.htm">Aktivitäten</a></li>
+                        <li><a href="links.htm">Links</a></li>
+    	           </ul>
+                </div>
+            </div>
+            <div id="mittlerespalte">
+                <div id="innerespalte">
+    	           <p>
+                        <h5>Sie befinden sich hier: > Home</h5>
+                    </p>
+                    <p>
+                        <h1>Herzlich Willkommen auf der Seite des Rheydter Magdrigalchores!</h1>
+                    </p>
+    		        <p>Wir sind ein Chor, der sich aus 26 Hobbysängerinnen und Hobbysängern zusammensetzt und schon seit 1966 diesen Namen trägt. </p>
+                    <p>Unser Repertoire reicht von klassischen Werken und weltlicher Musik bis hin zu Spirituals. Wir singen mit Leib und Seele und trauen uns auch an anspruchsvolle Chorwerke heran. </p>
+    		        <p>Genauso wichtig ist uns die Gemeinschaft, die wir durch gemeinsame Chorfahrten und Aktivitäten pflegen.</p> 
+                    <p>Denn das Singen soll vor allem Spaß machen – nur so können wir uns und unser Publikum begeistern. </p>
+    		        <img src="../bilder/finale%20Fotos/Home/home2.jpg">
+                </div>
+            </div>
+
+            <div id="fuss">
+                <div id="fussinnen">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
